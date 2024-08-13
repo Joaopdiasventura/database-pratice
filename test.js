bcd66059223b3898a4900ec59c15a042cb71cd64
João Paulo Dias <joaopdiasventura@gmail.com>
2024-08-13 14:49:48 -0300

    refactor: removing the test

--- test.js
+++
@@ -1,8 +0,0 @@
-import { hashSync, compareSync } from 'bcrypt';
-
-const password = '123'; // Senha original
-const hash = hashSync(password, 10); // Criação do hash
-console.log('Hash gerado:', hash);
-
-const isMatch = compareSync(password, hash); // Verificação da senha
-console.log('A senha corresponde?', isMatch); // Deve ser true
